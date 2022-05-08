c819e0c80e24e562b1a9fd5a9c395d2b47152599
rahim einollahi <r.einollahi@gamil.com>
2022-05-08 22:02:49 +0430

    installed bootstrap framework

--- src/main.js
+++ src/main.js
@@ -2,4 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+/****************************************************************************/
+// insatlled bootstrap and add dependency files
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+/****************************************************************************/
 createApp(App).use(router).mount('#app')
