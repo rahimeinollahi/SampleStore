bf5a6dff6cc807d3d4bf4fa75883fc55d2ad71ce
rahim einollahi <r.einollahi@gamil.com>
2022-05-09 19:00:56 +0430

    iransite is added to project

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,13 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/iransite',
+    name: 'IranSite',
+    component: () => import(/* webpackChunkName: "iransite" */ '../views/IranSite.vue'),
+    meta:{transition:'ts-iransite'}
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/IranSite.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container mt-2">
+        <div class="row">
+            <div class="col-sm-1">
+                
+            </div>
+                <h6 class="display-6 text-end mx-3">طراحی سایت با ایران سایت اولین شرکت طراحی سایت ایران</h6>
+            <div class="col-sm-10 d-flex ">
+                <p class="text-end mx-5 px-2">
+                    ایران سایت قدیمی ترین شرکت طراحی سایت در ایران فعالیت خود را از سال 1379 به صورت تخصصی در زمینه طراحی وب سایت ، سئو و دیجیتال مارکتینگ آغاز کرد و در حال حاضر با بیش از 20 سال سابقه در این زمینه و بیشتر از 2 هزار نمونه کار طراحی سایت موفق آماده ارائه خدمات به مشتریان گرامی می باشد ، در ایران سایت همیشه اولویت با کیفیت و ارائه جدید ترین روش های طراحی سایت بوده است ، ایران سایت به دلیل داشتن چندین دپارتمان در زمینه طراحی سایت ، سئو ، دیجیتال مارکتینگ ، سوشال مدیا ، عکاسی ، گوگل ادوردز ، تولید محتوا و برنامه نویسی همراه با نیرو های متخصص و با تجربه انتخاب خوبی برای داشتن همراهی مطمئن برای شروع کسب و کار در بستر اینترنت می باشد.
+                </p>
+                <img src="../../src/assets/images/ticket-content.jpg" alt="ticket_image" class="rounded" >
+            </div>
+            <div class="col-sm-1">
+            </div>
+        </div>
+        <div class="d-flex justify-content-center">
+            <button class="btn btn-info flex-sm-grow-3   mx-3 mt-3 mb-3 p-3"> <i class="fa fa-star" /> نمونه کارهای طراحی سایت</button>
+            <button class="btn btn-success  flex-sm-grow-1 mx-3 mt-3 mb-3 p-3"> <i class="fa fa-save" /> ثبت درخواست</button>
+        </div>
+
+            <div class="mb-3 mt-3">
+                <span class="border-bottom w-100">________________________________________________________</span>
+                <span class="mx-3">طراحی وب سایت</span>
+                <span class="border-bottom w-100">________________________________________________________</span>
+            </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.px-2 {
+ letter-spacing: 1px;
+ line-height: 3;
+
+}
+</style>
