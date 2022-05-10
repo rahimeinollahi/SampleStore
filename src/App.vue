29509a205c29d1c7943083e7233463addaf94e78
rahim einollahi <r.einollahi@gamil.com>
2022-05-10 10:48:29 +0430

    project done

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,17 @@
 
   </div>
   
-  <router-view/>
+  <!-- <router-view v-slot="{Component, route}">
+    <transition :name="route.meta.transition">
+      <component :is="Component">
+    </transition>
+  </router-view> -->
+
+  <router-view v-slot="{ Component, route }">
+   <transition :name="route.meta.transition || 'ts-fade'">
+    <component :is="Component" />
+  </transition>
+</router-view>
 </template>
 
 <script>
@@ -38,5 +48,63 @@ export default {
       color: #42b983;
     }
   }
+}
+
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(50px);
+  opacity: 0;
+  background-color: red;
+}
+.ts-home-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.ts-home-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.ts-home-enter-from,
+.ts-home-leave-to {
+  transform: translateX(50px);
+  opacity: 0;
+  background-color: red;
+}
+.ts-about-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.ts-about-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.ts-about-enter-from,
+.ts-about-leave-to {
+  transform: translateX(50px);
+  opacity: 0;
+  background-color: red;
+}
+
+.ts-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.ts-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.ts-fade-enter-from,
+.ts-fade-leave-to {
+  transform:scale(2);
+  opacity: 0;
+  background-color: red;
 }
 </style>
